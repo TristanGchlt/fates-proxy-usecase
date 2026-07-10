bd565636809d038969d94aa842aa66bf7e2e9ffc
Tristan Gouaichault <tristangchlt@gmail.com>
2026-07-10 14:16:27 +0200

    exécution de pipeline en ne lisant que les données brutes

--- pipeline.py
+++ pipeline.py
@@ -6,17 +6,17 @@ import mlflow.sklearn
 PROJECT_ROOT = Path(__file__).resolve().parents[0]
 sys.path.append(str(PROJECT_ROOT))
 
-from scripts.clean_data import main as clean
-from scripts.prepare_data import main as split
-from scripts.model_training import main as train
-from src.utils import read_config, clean_folder
+from src.data.data_cleaning import clean
+from src.data.data_preparation import split
+from src.models.training import train
+from src.models.utils import predict
+from src.metrics.utils import compute_measures
+from src.utils import read_config, clean_folder, load_csv
 from src.models.utils import save_model, save_model_type
 
 RAW_DATA_FILE = PROJECT_ROOT / "data" / "raw" / "data.csv"
-CLEAN_DATA_PATH = PROJECT_ROOT / "data" / "processed" / "processed_data.csv"
 
 CONFIG_PATH = PROJECT_ROOT / "config" / "config.yaml"
-SPLIT_PATH = PROJECT_ROOT / "data" / "split"
 
 PROD_PATH = PROJECT_ROOT / "model"
 
@@ -29,42 +29,74 @@ def run_pipeline() :
     config = read_config(CONFIG_PATH)
     model_name = config['model_name']
 
+    # Chargement des données brutes
+    dataset = load_csv(RAW_DATA_FILE)
+
     with mlflow.start_run(run_name=model_name):
 
         ####
         # Nettoyage et préparation des données
         ####
 
-        # Nettoyage : Adresse du fichier des données brut et écrit le fichier de données nettoyées.
-        clean(raw_data_file=RAW_DATA_FILE,
-              output_path=CLEAN_DATA_PATH)
+        # Nettoyage
+        clean_dataset = clean(dataset)
         
-        # Préparation des données : Récupère le fichier des données nettoyées, applique les transformations selon la config, écrit le fichier des données splitées.
-        split_logs = split(config_path=CONFIG_PATH,
-                            processed_data_path=CLEAN_DATA_PATH,
-                            split_path=SPLIT_PATH)
+        # Préparation des données
+        X_train, y_train, p_train, X_test, y_test, p_test = split(clean_dataset, 
+                                                              test_size = config['test_size'], 
+                                                              protected_feature = config['protected_feature'], 
+                                                              split_strategy = config['f_split_strategy'], 
+                                                              seed = config['split_random_seed'], 
+                                                              target_feature = config['target_feature'], 
+                                                              balance_strategy = config['f_balance'], 
+                                                              balance_seed = config['f_balance_seed'], 
+                                                              hide_protected = config["f_hide_protected"])
         
+
         # Suivi de la config dans mlflow
+        split_logs = {
+            "test_size" : config['test_size'],
+            "protected_feature" : config['protected_feature'], 
+            "split_strategy" : config['f_split_strategy'], 
+            "seed" : config['split_random_seed'], 
+            "target_feature" : config['target_feature'], 
+            "balance_strategy" : config['f_balance'], 
+            "balance_seed" : config['f_balance_seed'], 
+            "hide_protected" : config["f_hide_protected"]
+        }
         for key, value in split_logs.items():
             mlflow.log_param(key, value)
 
         ####
         # Entrainement et évaluation du modèle
         ####
+
+        # Entrainement
+        model_type = config['model_type']
+        hyperparameters = config['model_hyperparameters']
+        model = train(model_type, X_train, y_train, hyperparameters)
         
-        # Récupère les données split et entraine le modele
-        model_logs = train(config_path=CONFIG_PATH,
-                            split_path=SPLIT_PATH)
+        # prediction
+        y_pred = predict(model, model_type, X_test)
+
+        # evaluation
+        metrics = config['metrics']
+        data = {
+            "y_test" : y_test,
+            "y_pred" : y_pred,
+            "p_test" : p_test
+        }
+        measures = compute_measures(data, metrics)
         
         # Suivi des paramètres du modèles dans mlflow
-        mlflow.log_params(model_logs['hyperparameters'])
+        mlflow.log_params(hyperparameters)
 
         # Evaluation du modele et suivi dans mlflow
-        for metric, value in model_logs["measures"].items():
+        for metric, value in measures.items():
             mlflow.log_metric(metric, value)
 
         # Suivi du modele lui même dans mlflow
-        mlflow.sklearn.log_model(model_logs['model'], 
+        mlflow.sklearn.log_model(model, 
                                  name="model")
         
     ####
@@ -72,13 +104,11 @@ def run_pipeline() :
     ####
         
     if config['prod'] :
-        model = model_logs['model']
-        model_type = config['model_type']
         clean_folder(PROD_PATH)
         save_model(model, model_type, PROD_PATH)
         save_model_type(model_type, PROD_PATH)
-        
-    return model_logs
+    
+    return 0
 
 if __name__ == "__main__" :
     run_pipeline()
--- src/models/training.py
+++ src/models/training.py
@@ -1,16 +1,14 @@
 
 
-def train(model_type, data, hyperparameters) :
+def train(model_type, X_train, y_train, hyperparameters) :
     trainers = {
         "Random Forest Classifier" : train_rfc
     }
-    return trainers[model_type](data, hyperparameters)
+    return trainers[model_type](X_train, y_train, hyperparameters)
 
 
-def train_rfc(data, hyperparameters) :
+def train_rfc(X_train, y_train, hyperparameters) :
     from sklearn.ensemble import RandomForestClassifier
-    X_train = data['X_train']
-    y_train = data['y_train'].values.ravel()
     model = RandomForestClassifier(**hyperparameters)
     model.fit(X_train, y_train)
     return model
