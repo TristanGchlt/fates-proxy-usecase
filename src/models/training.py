af2abe4327369dfd30717e901699ae11d5b24d3b
Tristan Gouaichault <tristangchlt@gmail.com>
2026-08-17 11:14:38 +0200

    Xgboost as available model

--- src/models/training.py
+++ src/models/training.py
@@ -2,7 +2,8 @@
 
 def train(model_type, X_train, y_train, hyperparameters) :
     trainers = {
-        "Random Forest Classifier" : train_rfc
+        "Random Forest Classifier" : train_rfc,
+        "XGBoost" : train_xgboost
     }
     return trainers[model_type](X_train, y_train, hyperparameters)
 
@@ -10,5 +11,11 @@ def train(model_type, X_train, y_train, hyperparameters) :
 def train_rfc(X_train, y_train, hyperparameters) :
     from sklearn.ensemble import RandomForestClassifier
     model = RandomForestClassifier(**hyperparameters)
+    model.fit(X_train, y_train)
+    return model
+
+def train_xgboost(X_train, y_train, hyperparameters) :
+    from xgboost import XGBClassifier
+    model = XGBClassifier(**hyperparameters)
     model.fit(X_train, y_train)
     return model
